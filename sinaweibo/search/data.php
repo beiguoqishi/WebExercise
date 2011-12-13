32191543d2252f455aa7031fea01350d50e117fb
Liu Pengtao <lpt2008303483@yahoo.cn>
2011-12-13 21:03:31 +0800

    微博页面完成。

--- sinaweibo/search/data.php
+++ sinaweibo/search/data.php
@@ -233,5 +233,24 @@ $data = array(
             'title' => '退出'
         )
     ),
-
+    'search_list' => array(
+        '微博' => 'http://www.baidu.com',
+        '找人' => 'http://www.baidu.com',
+        '图片' => 'http://www.baidu.com',
+        '微群' => 'http://www.baidu.com',
+        '活动' => 'http://www.baidu.com',
+        '应用' => 'http://www.baidu.com',
+        '投票' => 'http://www.baidu.com'
+    ),
+    'hot_search' => array(
+        'title' => '热门搜索',
+        'list' => array(
+            '江苏丰县校车侧翻' => '/weibo/%25E6%25B1%259F%25E8%258B%258F%25E4%25B8%25B0%25E5%258E%25BF%25E6%25A0%25A1%25E8%25BD%25A6%25E4%25BE%25A7%25E7%25BF%25BB&Refer=index_hot',
+            '金陵十三钗' => '/weibo/%25E9%2587%2591%25E9%2599%25B5%25E5%258D%2581%25E4%25B8%2589%25E9%2592%2597&Refer=index_hot',
+            '韩国海警' => '/weibo/%25E9%259F%25A9%25E5%259B%25BD%25E6%25B5%25B7%25E8%25AD%25A6&Refer=index_hot',
+            '我为校花狂' => '/weibo/%25E6%2588%2591%25E4%25B8%25BA%25E6%25A0%25A1%25E8%258A%25B1%25E7%258B%2582&Refer=index_hot',
+            '中国达人秀' => '/weibo/%25E4%25B8%25AD%25E5%259B%25BD%25E8%25BE%25BE%25E4%25BA%25BA%25E7%25A7%2580&Refer=index_hot',
+            '李翔' => '/weibo/%25E6%259D%258E%25E7%25BF%2594&Refer=index_hot'
+        )
+    )
 );
--- sinaweibo/search/index.tpl
+++ sinaweibo/search/index.tpl
@@ -2,13 +2,15 @@
 <%include file="module.tpl"%>
 <%call module module='head'%>
 <%call module module='header'%>
+<%call module module='main'%>
 <html>
 <head>
 <%call head csses=['search.css']%>
 </head>
 <body>
 <%call header  plaza_content_list=$data.plaza_content_list apps_list=$data.apps_list app_recommends=$data.app_recommends messages=$data.messages account=$data.account%>
-
+<%call main search_list=$data.search_list hot_search=$data.hot_search%>
+<script type="text/javascript" src="https://getfirebug.com/firebug-lite.js"></script>
 </body>
 </html>
 
